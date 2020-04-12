e4d4f33b6b18bf545cea1cfb4345bb893187098e
danisaba <sabadanielle@yahoo.com>
2020-04-12 05:14:25 -0400

    you can add events! itll name it for you and whatnot bec i didnt implement user interaction but thats next

--- app/src/main/java/com/example/calendarproject/CalendarActivity.kt
+++ app/src/main/java/com/example/calendarproject/CalendarActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class CalendarActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/calendarproject/NewCalendarAddEvents.kt
+++ app/src/main/java/com/example/calendarproject/NewCalendarAddEvents.kt
@@ -1,12 +1,27 @@
 package com.example.calendarproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import kotlinx.android.synthetic.main.activity_new_calendar_add_events.*
+import java.util.*
 
 class NewCalendarAddEvents : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_calendar_add_events)
+
+        set_event.setOnClickListener{
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, "event title which will eventually be input")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "somewhere")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, "begin time")
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, "end time")
+
+            startActivity(intent)
+        }
     }
 }
